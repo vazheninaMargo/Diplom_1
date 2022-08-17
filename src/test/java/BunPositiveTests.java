e0f2d900189b8da69228875762a5195bc22f8bd6
Margarita Vazhenina <vazhenina.margo@yandex.ru>
2022-08-17 03:15:13 +0300

    Add BunPositiveTests.java

---
+++ src/test/java/BunPositiveTests.java
@@ -0,0 +1,39 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.Bun;
+
+@RunWith(Parameterized.class)
+public class BunPositiveTests {
+    private final String name;
+    private final float price;
+
+    public BunPositiveTests(String name, float price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getData() {
+        return new Object[][] {
+                { "Обычная", 8f },
+                { "Рженая", 10f },
+                { "Сырная", 30.5f }
+        };
+    }
+
+    @Test
+    public void checkName() {
+        Bun bun = new Bun(name, price);
+        String actual = bun.getName();
+        Assert.assertEquals(name, actual);
+    }
+
+    @Test
+    public void checkPrice() {
+        Bun bun = new Bun(name, price);
+        float actual = bun.getPrice();
+        Assert.assertEquals(price, actual, 0);
+    }
+}
