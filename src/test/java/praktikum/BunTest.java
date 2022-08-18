18a36ae48747364ced1e3b3d7984104e0d8b7a10
Margarita Vazhenina <vazhenina.margo@yandex.ru>
2022-08-18 03:22:22 +0300

    Rename BunTests with BunTest. Refactoring of BunTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,15 +1,16 @@
+package praktikum;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import praktikum.Bun;
 
 @RunWith(Parameterized.class)
-public class BunTests {
+public class BunTest {
     private final String name;
     private final float price;
 
-    public BunTests(String name, float price) {
+    public BunTest(String name, float price) {
         this.name = name;
         this.price = price;
     }
@@ -24,16 +25,24 @@ public class BunTests {
     }
 
     @Test
-    public void checkName() {
+    public void getName() {
         Bun bun = new Bun(name, price);
         String actual = bun.getName();
         Assert.assertEquals(name, actual);
     }
 
     @Test
-    public void checkPrice() {
+    public void getPrice() {
         Bun bun = new Bun(name, price);
         float actual = bun.getPrice();
         Assert.assertEquals(price, actual, 0);
     }
+
+//    @Test
+//    public void getName() {
+//    }
+//
+//    @Test
+//    public void getPrice() {
+//    }
 }
